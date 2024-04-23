ce80122eb079e71fe942a4afaa0152c5dd723ecc
Sumit Kumar <j4sumit@gmail.com>
2024-04-23 18:20:41 +0530

    15 Display Product List with API

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-home',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller-home.component.css']
 })
 export class SellerHomeComponent implements OnInit {
-
-  constructor() { }
+productList:undefined| product[]
+  constructor(private product: ProductService) { }
 
   ngOnInit(): void {
+    this.product.productList().subscribe((result)=>{
+console.log(result)
+this.productList=result
+    })
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,4 +11,8 @@ export class ProductService {
   addProduct(data:product){
 return this.http.post("http://localhost:3000/products",data)
   }
+  productList()
+  {
+    return this.http.get<product[]>("http://localhost:3000/products")
+  }
 }
